4c3d90b84afa502e41485e535a97e4d11181d7eb
Hrishikesh Makwana <hrishi1563@gmail.com>
2024-06-21 17:15:16 +0530

    Delete button added

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useRef } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 export default function Home() {
   const titleRef = useRef();
@@ -25,12 +26,26 @@ export default function Home() {
     }
     titleRef.current.value = "";
     descriptionRef.current.value = "";
-  };
 
+    setModal(false);
+    localStorage.setItem("name", tasks);
+  };
+  const handleDelete = (taskID) => {
+    setTasks((prevState) => {
+      return prevState.filter((i) => i.id != taskID);
+    });
+    localStorage.setItem("name", tasks);
+  };
   return (
     <div className="w-full h-full flex flex-row border items-center">
       {modal && (
-        <div className="absolute flex flex-col border border-red-500 bg-blue-800 rounded-3xl w-[50vh] h-[60vh] my-auto mx-[80vh]">
+        <div className="absolute flex flex-col border border-black bg-blue-800 rounded-3xl w-[50vh] h-[60vh] my-auto mx-[80vh]">
+          <button
+            className="self-start p-4 text-xl cursor-pointer hover:text-2xl"
+            onClick={() => setModal(false)}
+          >
+            X{" "}
+          </button>
           <form onSubmit={handleCreateTask} className="p-4 flex flex-col gap-2">
             <div className="flex flex-col">
               <label htmlFor="title" className="font-bold text-white">
@@ -58,7 +73,7 @@ export default function Home() {
             </div>
             <button
               type="submit"
-              className="self-start mx-8 my-4 border px-3 py-2 bg-green-400 rounded-2xl"
+              className="self-start mx-8 my-4 border px-3 py-2 bg-green-400 rounded-2xl hover:bg-red-500"
             >
               Create
             </button>
@@ -81,13 +96,21 @@ export default function Home() {
         </p>
         <ul className="flex flex-col mt-2 px-14">
           {tasks.map((element) => (
-            <li key={element.id}>
-              <h2 className="underline font-bold">
-                {"=>"}
-                {element.title}
-              </h2>
-              <p className="px-9 mt-2 overflow-clip">{element.description}</p>
-            </li>
+            <div key={element.id} className="flex flex-row justify-between">
+              <li>
+                <h2 className="underline font-bold">
+                  {"=>"}
+                  {element.title}
+                </h2>
+                <p className="px-9 mt-2 overflow-clip">{element.description}</p>
+              </li>
+              <FaRegTrashAlt
+                className="hover:text-red-500"
+                onClick={() => {
+                  handleDelete(element.id);
+                }}
+              />
+            </div>
           ))}
         </ul>
       </main>
